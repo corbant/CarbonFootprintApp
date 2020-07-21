12f356b3d99d0c7b4a911f3dbb5092f018c5113a
corbant <corbant25@gmail.com>
2020-07-21 10:22:55 -0700

    Added Microcharts and added chart to graph page

--- CarbonFootprintApp/GraphPage.xaml.cs
+++ CarbonFootprintApp/GraphPage.xaml.cs
@@ -6,15 +6,39 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Microcharts;
+using Entry = Microcharts.Entry;
+using SkiaSharp;
 
 namespace CarbonFootprintApp
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class GraphPage : ContentPage
     {
+
+        List<Entry> entries = new List<Entry>() {
+            new Entry(10) {
+                Label = "5/2/20",
+                Color = SKColor.Parse("#ebe834"),
+                ValueLabel = "10"
+            },
+            new Entry(8)
+            {
+                Label = "5/9/20",
+                Color = SKColor.Parse("#34c3eb"),
+                ValueLabel = "8"
+            },
+            new Entry(7)
+            {
+                Label = "5/16/20",
+                Color = SKColor.Parse("#eb34cc"),
+                ValueLabel = "7"
+            }
+        };
         public GraphPage()
         {
             InitializeComponent();
+            chart.Chart = new LineChart() { Entries = entries };
         }
     }
 }
